684cebacc5476a1a59a68694ce33f148e4a560c5
dodo <dodofory@qq.com>
2024-07-26 09:23:46 +0800

    All Done!

--- exercises/20_class_template/main.cpp
+++ exercises/20_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4;i++) { 
+            shape[i] = shape_[i]; 
+            size *= shape_[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,36 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        bool flag = true;
+        for (int i = 0; i < 4; i++) {
+            if ( (shape[i] != others.shape[i]) && (others.shape[i] != 1) ) {
+                flag = false;
+                break;
+            }
+        }
+        unsigned int size = shape[0]*shape[1]*shape[2]*shape[3];
+        if(flag) {
+            for (unsigned int i = 0; i < size; ++i) {
+                unsigned int idx[4];
+                idx[3] = i % shape[3];
+                idx[2] = (i / shape[3]) % shape[2];
+                idx[1] = (i / (shape[3] * shape[2])) % shape[1];
+                idx[0] = (i / (shape[3] * shape[2] * shape[1])) % shape[0];
+
+                for (int j = 0; j < 4; j++) {
+                    if (others.shape[j] == 1) {
+                        idx[j] = 0;
+                    }
+                }
+                
+                unsigned int flat_idx_others = idx[0] * others.shape[1] * others.shape[2] * others.shape[3] +
+                                            idx[1] * others.shape[2] * others.shape[3] +
+                                            idx[2] * others.shape[3] +
+                                            idx[3];
+
+                data[i] += others.data[flat_idx_others];
+            }
+        }
         return *this;
     }
 };
--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -11,6 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < N; i++) {
+            shape[i] = shape_[i];
+            size *= shape_[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -35,7 +39,17 @@ private:
         for (unsigned int i = 0; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
+            if (i < N-1 ) {
+                unsigned int order = 1;
+                for (int j = i+1; j < N;j++) {
+                    order *= shape[j];
+                }
+                index += indices[i]*order;
+            } else {
+                index += indices[i];
+            }
         }
+            
         return index;
     }
 };
